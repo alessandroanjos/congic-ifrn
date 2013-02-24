59c519ad660aba77783723eaa9f2c7ccbda76325
sandro.adsc <sandro.adsc@gmail.com>
2013-02-24 18:31:01 +0000

    Add arquivos index.scala.html nos views.

---
+++ congic-ifrn-web/congic-ifrn/app/controllers/Sessions.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Classe responsÃ¡vel pelo controle da autenticaÃ§Ã£o dos usuÃ¡rios.
+ * 
+ * @author Alessandro
+ */
+
+public class Sessions extends Controller {
+
+}
