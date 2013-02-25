4f14f169c6f6aa526bfd2529794e0b0632677f78
sandro.adsc <sandro.adsc@gmail.com>
2013-02-25 13:19:08 +0000

    Add classe controladora Artigos. Alteração no routes.

---
+++ congic-ifrn-web/congic-ifrn/app/controllers/Artigos.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Artigos extends Controller{
+
+	public static Result index(){
+		return TODO;
+	}
+	
+	public static Result visualizar(Long id) {
+		return TODO;
+	}
+	
+	public static Result formulario() {
+		return TODO;
+	}
+	
+	public static Result submeter() {
+		return TODO;
+	}
+	
+	public static Result formularioEdicao(Long id) {
+		return TODO;
+	}
+	
+	public static Result editar(Long id) {
+		return TODO;
+	}
+	
+	public static Result deletar(Long id) {
+		return TODO;
+	}
+	
+	public static Result formularioAvaliacao(Long id) {
+		return TODO;
+	}
+	
+	public static Result avaliacaoArtigo(Long id) {
+		return TODO;
+	}
+	
+	public static Result meusArtigosAvaliados() {
+		return TODO;
+	}
+	
+	public static Result meusArtigos(){
+		return TODO;
+	}
+}
