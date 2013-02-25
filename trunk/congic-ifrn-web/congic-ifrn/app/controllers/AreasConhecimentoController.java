66c41a552119018f9532a8f49f12ef0bc6341a5f
sandro.adsc@gmail.com <sandro.adsc@gmail.com@8cb83436-9a26-e442-8692-fad5e6ea427f>
2013-02-25 12:54:39 +0000

    Add classes no Controllers e configuração do routes.

---
+++ trunk/congic-ifrn-web/congic-ifrn/app/controllers/AreasConhecimentoController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class AreasConhecimentoController extends Controller{
+
+	public static Result index(){
+		return TODO;
+	}
+	
+	public static Result visualizar(Long id){
+		return TODO;
+	}
+	
+	public static Result formulario() {
+		return TODO;
+	}
+	
+	public static Result cadastrar(){
+		return TODO;
+	}
+	
+	public static Result editar(Long id) {
+		return TODO;
+	}
+	
+	public static Result formularioEdicao(Long id) {
+		return TODO;
+	}
+	
+	public static Result deletar(Long id){
+		return TODO;
+	}
+}
